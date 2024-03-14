19a1371c391ebfff5bb49e2f2c2db766299d8411
doliva <doliva@icopartners.com>
2024-03-14 21:45:08 +0100

    First commit

---
+++ app/Models/Company.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Company extends Model
+{
+    use HasFactory;
+
+    protected $table = "companies";
+    protected $primarKey = "company_id";
+    public $incrementing = true;
+    public $timestamps = true;
+
+    protected $fillable = [
+        "name",
+    ];
+
+    /**
+     * Get all of the comments for the Company
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function teams(): HasMany
+    {
+        return $this->hasMany(Teams::class);
+    }
+}
---
+++ app/Models/Role.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Role extends Model
+{
+    use HasFactory;
+
+    protected $table = 'roles';
+    protected $primarKey = "role_id";
+    public $incrementing = true;
+    public $timestamps = true;
+
+
+    /**
+     * Get the user that owns the Role
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
